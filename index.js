e8b11b155773bbb9ee0c6d4ec7246d80d2c54cba
Kate Hudson <k88hudson@gmail.com>
2014-10-08 17:27:08 -0400

    add deps

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 module.exports = function () {
-    console.log('I love purple songs! How can a song be purple you say? Well, I will have show you some day...');
+    var randomWord = require('random-words');
+
+    // Make a song!
+    var mySong = randomWord() + ' ' + randomWord() + ' ' randomWord();
+
+    // Sing it!
+    console.log("Let's sing a purple song.");
+    console.log(mySong);
 }
