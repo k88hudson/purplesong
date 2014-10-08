16df2a07e7d2cc4ad6d647b1ff76c1fa3672a7fe
Kate Hudson <k88hudson@gmail.com>
2014-10-08 17:41:31 -0400

    add some speakin

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ module.exports = function () {
 
     // This is where we import our random-words program
     var randomWord = require('random-words');
+    var say = require('say');
 
     // We can use it to make a song!
     // e.g. This might output:
     //      dog space pizza
-    var mySong = randomWord() + ' ' + randomWord() + ' ' randomWord();
+    var mySong = randomWord() + ' ' + randomWord() + ' ' + randomWord();
 
     // Sing it!
     console.log("Let's sing a purple song.");
-    console.log(mySong);
+    say.speak(null, mySong);
 }
